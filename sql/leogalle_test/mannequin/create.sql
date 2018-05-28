56161d6300d5822c0775a6663b7782f80a2e938e
Leo Galleguillos <leo.galleguillos@icloud.com>
2018-05-28 21:37:24 +0000

    Add translate and rotate columns to mannequin table

--- sql/leogalle_test/mannequin/create.sql
+++ sql/leogalle_test/mannequin/create.sql
@@ -1,6 +1,12 @@
 CREATE TABLE `mannequin` (
     `mannequin_id` int(10) unsigned auto_increment,
-    `user_id` int(10) not null,
+    `user_id` int(10) unsigned not null,
+    `translate_x` FLOAT(8, 2) SIGNED,
+    `translate_y` FLOAT(8, 2) SIGNED,
+    `translate_z` FLOAT(8, 2) SIGNED,
+    `rotate_x` FLOAT(8, 2) SIGNED,
+    `rotate_y` FLOAT(8, 2) SIGNED,
+    `rotate_z` FLOAT(8, 2) SIGNED,
     `created` datetime not null,
     `updated` datetime default null,
     PRIMARY KEY (`mannequin_id`)
